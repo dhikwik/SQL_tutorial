9fdd09ebcc651c9d22f5ca80e894eacebc701867
unknown <19eucs030@skcet.ac.in>
2024-03-24 13:44:58 +0530

    First commit

---
+++ table.sql
@@ -0,0 +1,60 @@
+
+
+-- CREATE table Movies(
+--     movie_id INT PRIMARY KEY,
+--     Name VARCHAR(30),
+--     Released-Year YEAR,
+--     Hero VARCHAR(255),
+--     Collection INT,
+--     Languages VARCHAR(40),
+-- )
+
+-- CREATE table Actors(
+--     actor_id INT PRIMARY KEY,
+--     Name VARCHAR(30),
+--     Age INT,
+--     movie_id INT,
+--     Salary INT
+-- )
+
+-- -- Inserting data into the Movies table
+-- INSERT INTO Movies (movie_id, Name, Released_Year, Hero, Collection, Languages)
+-- VALUES (1, 'Inception', 2010, 'Leonardo DiCaprio', 830000000, 'English');
+
+--  VALUES (2, 'Parasite', 2019, 'Song Kang-ho', 266000000, 'Korean');
+
+--  VALUES (3, 'La La Land', 2016, 'Ryan Gosling', 445000000, 'English');
+
+--  VALUES (4, 'DDLJ', 1995, 'Shah Rukh Khan', 100000000, 'Hindi');
+
+--  VALUES (5, 'The Godfather', 1972, 'Marlon Brando', 246000000, 'English');
+
+--  VALUES (6, 'Titanic', 1997, 'Leonardo DiCaprio', 2208000000, 'English');
+
+--  VALUES (7, 'Interstellar', 2014, 'Matthew McConaughey', 677000000, 'English');
+
+--  VALUES (8, 'The Shawshank Redemption', 1994, 'Tim Robbins', 58300000, 'English');
+
+--  VALUES (9, 'Baahubali: The Beginning', 2015, 'Prabhas', 100000000, 'Telugu, Tamil');
+
+--  VALUES (10, 'Pulp Fiction', 1994, 'John Travolta', 214000000, 'English');
+
+-- -- Inserting data into the Actors table
+-- INSERT INTO Actors (actor_id, Name, Age, movie_id, Salary)
+-- VALUES (1, 'Leonardo DiCaprio', 46, 1, 20000000);
+
+--  VALUES (2, 'Song Kang-ho', 55, 2, 1000000);
+
+--  VALUES (3, 'Ryan Gosling', 41, 3, 15000000);
+
+--  VALUES (4, 'Shah Rukh Khan', 56, 4, 25000000);
+
+--  VALUES (5, 'Marlon Brando', NULL, 5, 500000);
+
+--  VALUES (6, 'Matthew McConaughey', 52, 7, 12000000);
+
+--  VALUES (7, 'Tim Robbins', 63, 8, 8000000);
+
+--  VALUES (8, 'Prabhas', 42, 9, 5000000);
+
+--  VALUES (9, 'John Travolta', 68, 10, 15000000);
